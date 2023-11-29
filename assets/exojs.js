9f0e60b87c12528f5f197f7730041723ae479cbb
cybtetuanui92 <mateau.tetuanui@gmail.com>
2023-11-29 16:10:23 +0100

    initial commit

---
+++ assets/exojs.js
@@ -0,0 +1,76 @@
+//console.log("Bonjour toi!");
+
+//let resultat = 7 + 3;
+//console.log(resultat)
+
+//let nom = prompt("Quel est ton nom ?");
+//console.log(nom)
+//console.log("Bonjour " + nom + "!")
+
+//let number = [1, 2, 3, 4, 5];
+//for(let i = 5; i > 0; i--){
+//     console.log(i);
+//  }
+
+//let numberUser = prompt("Donnez un nombre ?");
+    //         if (numberUser%2 == 0) {
+  //      console.log("C'est un nombre pair");    
+//} 
+ //    else {console.log("C'est un nombre impair");
+//}
+//console.log("Bonjour " + nom + "!")
+   
+//let nombre = prompt("Choisis un nombre :");
+//for (let i = 1; i <= 10; i++) {
+  //  let resultat = nombre * i;
+    //console.log(nombre + " x " + i + " = " + resultat);
+//}
+// DOJO1 
+/*let array = ["Jay", "Camille", "Jean","Jeanne","Unix"]
+
+function likes(names) {
+if (names.length === 0) {
+  //  outpout : no one likes this
+  return "no one likes this"
+  console.log(likes(names));
+} 
+else if (names.length ===1) {
+  //  output : Jay likes this
+  return names[0]+ " likes this"
+}
+else if (names.length ===2) {
+  //  output : Jay, Camille likes this
+  return names[0]+", "+ names[1] + " likes this"
+}
+else {
+  //  output : Jay, Camille 2 likes this
+  return names[0]+", "+ names[1] +" and"+ (names.length - 2) + " likes this"
+}
+}
+*/
+
+
+console.log('Hello from assets/js/main.js');
+// calculette JS
+// Veuilles suivre les instructions dans le fichier instructions.md
+
+document.getElementById('calculate_button').addEventListener('click', event => {
+    event.preventDefault();
+    console.log('click')
+
+    let number1 = parseInt(document.getElementById('number1').value);
+    let number2 = parseInt(document.getElementById('number2').value);
+    let operator = document.getElementById('operator').value;
+    let result = 0;
+
+    if (operator === 'addition') {
+        result = number1 + number2
+    } else if (operator === 'soustraction') {
+        result = number1 - number2
+    } else if (operator === 'multiplication') {
+        result = number1 * number2 
+    } else {
+        result = number1 / number2
+    }
+    document.getElementById('result').innerHTML = result
+})
